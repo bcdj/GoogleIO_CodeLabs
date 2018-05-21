37066d95959a2ef1a172e7b4da6217b4237a902c
Chip Snyder <chip.snyder3@gmail.com>
2018-05-20 22:13:58 -0400

    update gitignore

---
+++ mlkit-android-custom-model/iOS/custom-model-codelab/custom-model-codelab/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  custom-model-codelab
+//
+//  Created by Chip Snyder on 5/20/18.
+//  Copyright © 2018 EisbarDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
